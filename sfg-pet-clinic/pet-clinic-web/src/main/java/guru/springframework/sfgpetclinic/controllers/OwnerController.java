0e81b23fb8e7b389ebef3e593ee1c06f3e420d96
Kiran Birajdar <kiranbirajdar58@gmail.com>
2018-09-30 01:13:37 +0530

    Added ownerList and updated controller closes #11

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,15 +1,25 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import guru.springframework.sfgpetclinic.services.OwnerService;
+
 @Controller
-@RequestMapping("/owners")//any request goes to owner folder will be handled by this class
+@RequestMapping("/owners") // any request goes to owner folder will be handled by this class
 public class OwnerController {
-	
-	@RequestMapping({"/index","/ownerList","index.html"})
-	public String listOwners() {
-		
+
+	private final OwnerService ownerService;
+
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
+
+	@RequestMapping({ "/index", "/ownerList", "index.html" })
+	public String listOwners(Model model) {
+		model.addAttribute("ownerList", ownerService.findAll());
 		return "owners/index";
 	}
 }

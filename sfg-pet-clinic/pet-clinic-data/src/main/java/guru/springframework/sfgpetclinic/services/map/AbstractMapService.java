b29619a20bb254affe87203c1a73e1942f33e9c1
Kiran Birajdar <kiranbirajdar58@gmail.com>
2018-09-30 10:06:16 +0530

    closes #18

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,12 +1,15 @@
 package guru.springframework.sfgpetclinic.services.map;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, ID> {
-	protected Map<ID, T> map = new HashMap<>();
+import guru.springframework.sfgpetclinic.model.BaseEntity;
+
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+	protected Map<Long, T> map = new HashMap<>();
 
 	public Set<T> findAll() {
 		return new HashSet<>(map.values());
@@ -16,17 +19,28 @@ public abstract class AbstractMapService<T, ID> {
 		return map.get(id);
 	}
 
-	public T save(ID id, T object) {
-		map.put(id, object);
+	public T save( T object) {
+		if(object != null) {
+			if(object.getId() == null){
+				object.setId(getNextId());
+				
+			}
+			map.put(object.getId(), object);
+		}
+		
 		return object;
 	}
-	
+
 	public void deleteById(ID id) {
-			map.remove(id);
-		}
-	public void delete(T object) {
-		map.entrySet().removeIf(entry->entry.getValue().equals(object));
-		
+		map.remove(id);
 	}
-	
+
+	public void delete(T object) {
+		map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+
+	}
+
+	private Long getNextId() {
+		return Collections.max(map.keySet())+1;
+	}
 }
--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -31,7 +31,7 @@ public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetS
 	@Override
 	public Pet save(Pet object) {
 		// TODO Auto-generated method stub
-		return super.save(object.getId(), object);
+		return super.save( object);
 	}
 	
 	

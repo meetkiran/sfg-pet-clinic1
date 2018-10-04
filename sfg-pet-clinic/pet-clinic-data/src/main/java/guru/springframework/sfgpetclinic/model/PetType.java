a74b86d43a297532f6c1a09b65d1d4b915328500
Kiran Birajdar <kiranbirajdar58@gmail.com>
2018-10-04 07:01:16 +0530

    closes #21

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,6 +1,10 @@
 package guru.springframework.sfgpetclinic.model;
 
 public class PetType extends BaseEntity{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String name;
 
 	public String getName() {

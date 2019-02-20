fef87aef93c6ae7cbf584d839809357a048c5e37
Kiran Birajdar <kiranbirajdar58@gmail.com>
2019-02-20 07:29:42 +0530

    Check branch

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -17,7 +17,7 @@ import lombok.Setter;
 @Builder
 @Entity
 @Table(name = "types")
-public class PetType extends BaseEntity{
+public class PetType extends BaseEntity {
 	@Column(name = "name")
 	private String name;
 
